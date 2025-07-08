996bcf578843384abf0b542dd4b15aa744060b67
Pedro Carvalho <plucas682@gmail.com>
2025-07-08 17:28:26 -0300

    componentes footer, banner e about (html)

---
+++ src/app/components/homepage/category-banner/category-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-banner',
+  imports: [],
+  templateUrl: './category-banner.component.html',
+  styleUrl: './category-banner.component.css'
+})
+export class CategoryBannerComponent {
+
+}
