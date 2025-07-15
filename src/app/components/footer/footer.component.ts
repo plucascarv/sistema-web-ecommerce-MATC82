964719eafabc8e05b34a9a22531302af31f3fbfd
Isabela <plucas682@gmail.com>
2025-07-14 21:17:26 -0300

    css para footer e about componentes + minor fixes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
@@ -13,6 +14,13 @@ export class FooterComponent {
   categories:string[] = ['Notebooks', 'Desktops', 'Controles', 'Teclados', 'Mouses e Mouspeads', 'Monitores', 'Acessórios', 'Jogos', 'Peças'];
 
   navigateToPage(item:string) {
-    this.router.navigate(['/categories',item])
+    this.router.navigate(['/categories',item]);
   }
+
+  socials:string[] = [
+    "../../../../public/socials/twitter.svg",
+    "../../../../public/socials/instagram.svg",
+    "../../../../public/socials/facebook.svg",
+    "../../../../public/socials/whatsapp.svg",
+    "../../../../public/socials/youtube.svg"];
 }
--- src/app/components/homepage/about/about.component.ts
+++ src/app/components/homepage/about/about.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
 }
