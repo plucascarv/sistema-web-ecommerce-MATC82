40522bf033f55761205d9f414feb6410405b1ddd
Isabela <plucas682@gmail.com>
2025-07-15 18:35:41 -0300

    brands-categories + middleline em home component

--- src/app/components/homepage/brands-categories/brands-categories.component.ts
+++ src/app/components/homepage/brands-categories/brands-categories.component.ts
@@ -10,7 +10,17 @@ import { Router } from '@angular/router';
 export class BrandsCategoriesComponent {
   constructor(private router: Router) {}
   
-  brands:string[] = []
+  brands:string[] = [
+    "../../../../../public/brands/amd.svg",
+    "../../../../../public/brands/positivo.svg",
+    "../../../../../public/brands/corsair.svg",
+    "../../../../../public/brands/alienware.svg",
+    "../../../../../public/brands/logitech.svg",
+    "../../../../../public/brands/msi.svg",
+    "../../../../../public/brands/razer.svg",
+    "../../../../../public/brands/nvidia.svg"
+  ]
+
   categories = [
     {name: "Notebooks", img:''},
     {name: "PCs Gamer", img:''},
